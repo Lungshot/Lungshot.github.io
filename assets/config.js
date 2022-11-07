c97e324258d2f75254b0435ae561682008480a0c
Lungshot <86326515+Lungshot@users.noreply.github.com>
2022-11-07 13:16:38 -0600

    Update config.js
    updated links

--- assets/config.js
+++ assets/config.js
@@ -5,48 +5,48 @@ let config = {
   "links": [
     {
       "Title": "Facebook",
-      "URL": "http://www.harindu.dev",
-      "icon_classes": "fas fa-link",
+      "URL": "https://www.facebook.com/Lungsh0t",
+      "icon_classes": "fas fa-facebook",
     },
     {
       "Title": "Instagram",
-      "URL": "https://github.com/harindulk",
-      "icon_classes": "fab fa-github",
+      "URL": "https://www.instagram.com/kent_on_bass/",
+      "icon_classes": "fab fa-instagram",
     },
     {
       "Title": "LinkedIn",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
+      "URL": "https://www.linkedin.com/in/kentscherler/",
+      "icon_classes": "fab fa-linkedin"
     },
     {
       "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
+      "URL": "https://www.youtube.com/channel/UCBSSx_jEO-Zsv_yD924xhHg",
       "icon_classes": "fab fa-youtube"
     },
     {
       "Title": "Twitch",
       "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
-      "icon_classes": "fab fa-youtube"
+      "icon_classes": "fab fa-twitch"
     },
     {
       "Title": "Twitter",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
+      "URL": "https://twitter.com/Lungsh0t",
+      "icon_classes": "fab fa-twitter"
     },
     {
       "Title": "Cashapp",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
+      "URL": "https://cash.app/$iiidksiii",
+      "icon_classes": "fa-solid fa-money-check-dollar"
     },
     {
       "Title": "Venmo",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
+      "URL": "https://www.venmo.com/u/dksiii",
+      "icon_classes": "<i class="fa-solid fa-money-check-dollar"></i>"
     },
     {
       "Title": "Zelle",
       "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
+      "icon_classes": "fa-solid fa-money-check-dollar"
     },
   ]
 }
