0e96f9d046f704eeb64d97756c6df06487a94fe6
Lungshot <86326515+Lungshot@users.noreply.github.com>
2022-11-07 12:02:38 -0600

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,47 +1,52 @@
 let config = {
-  "Title": "Your Name - Links",
-  "Name": "Your Name",
-  "Description": "Your Description",
+  "Title": "Links",
+  "Name": "Kent Scherler",
+  "Description": "Network Engineer / Musician",
   "links": [
     {
-      "Title": "Website",
+      "Title": "Facebook",
       "URL": "http://www.harindu.dev",
       "icon_classes": "fas fa-link",
     },
     {
-      "Title": "Github",
+      "Title": "Instagram",
       "URL": "https://github.com/harindulk",
       "icon_classes": "fab fa-github",
     },
     {
-      "Title": "Play Store",
+      "Title": "LinkedIn",
       "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
       "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
-      "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
     },
     {
       "Title": "Youtube",
       "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
       "icon_classes": "fab fa-youtube"
+    },
+    {
+      "Title": "Twitch",
+      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
+      "icon_classes": "fab fa-youtube"
+    },
+    {
+      "Title": "Twitter",
+      "URL": "https://harindulk.itch.io/",
+      "icon_classes": "fab fa-itch-io"
+    },
+    {
+      "Title": "Cashapp",
+      "URL": "blog-test",
+      "icon_classes": "fas fa-blog"
+    },
+    {
+      "Title": "Venmo",
+      "URL": "https://twitter.com/Harindu_Fonseka",
+      "icon_classes": "fab fa-twitter"
+    },
+    {
+      "Title": "Zelle",
+      "URL": "https://www.linkedin.com/in/harindulk/",
+      "icon_classes": "fab fa-linkedin"
     },
   ]
 }
